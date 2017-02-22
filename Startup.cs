b79867424939ef8458bc0cae80ea46fe86f48e28
Bill Berger <bill.berger@csiweb.com>
2017-02-22 12:50:52 -0500

    remove unused using directive

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace CoreWebTest
 {
